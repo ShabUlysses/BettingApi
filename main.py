4f29a609a81097080d15f277fbbf2c3408b54575
ishabelnikov <ishabelnikov@neuro.net>
2024-05-15 17:27:54 +0300

    add project structure

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-from fastapi import FastAPI
-
-app = FastAPI()
+import uvicorn
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+if __name__ == "__main__":
+    uvicorn.run("app.app:app", host="localhost", port=8000, reload=True)
